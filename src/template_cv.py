5b611db358e38203809f535bf9b3d315e7d19167
rodrigo <rodrigo.vega23@gmail.com>
2022-09-06 16:00:51 -0300

    detectar pato rectangulo

--- src/template_cv.py
+++ src/template_cv.py
@@ -21,14 +21,18 @@ class Template(object):
 
 	#def callback(self,msg):
 
-	def procesar_img(self, msg):
+	def procesar_img(self, imsg):
 		# Cambiar espacio de color
 		bridge=CvBridge()
 		image_in= bridge.imgmsg_to_cv2(msg, "bgr8")				
 		image_out= cv2.cvtColor(image_in,cv2.COLOR_BGR2HSV)
-		lower_limit=np.array([20,40,200])
-		upper_limit=np.array([40,255,255])
+		lower_limit=np.array([20,100,200])
+		upper_limit=np.array([60,255,255])
 		mask=cv2.inRange(image_out,lower_limit,upper_limit)
+		kernel= np.ones((5,5), np.uint8)
+		mask = cv2.erode(mask,kernel, iterations = 1)
+		mask = cv2.dilate(mask,kernel, iterations = 2)
+		image_od=cv2.bitwise_and(image_in,image_in,mask=mask)
 		# Filtrar rango util
 		
 
@@ -37,11 +41,19 @@ class Template(object):
 		# Aplicar transformaciones morfologicas
 
 		# Definir blobs
-
+		
+                _,contours, hierarchy=cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+                for cnt in contours:    
 		# Dibujar rectangulos de cada blob
-
+                  x,y,w,h=cv2.boundingRect(cnt)
+                
+                  if cv2.contourArea(cnt) < 200:
+                	continue 
+                	
+                  cv2.rectangle(image_od,(x,y),(x+w,y+h),(0,0,255),2)
+                
 		# Publicar imagen final
-		msg = bridge.cv2_to_imgmsg(image_out, "bgr8")
+		msg = bridge.cv2_to_imgmsg(image_od, "bgr8")
 	        self.publisher.publish(msg)
 
 def main():
