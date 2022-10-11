34c145b0526905d1819ff5e778c9fe2005f56eda
rodrigo <rodrigo.vega23@gmail.com>
2022-10-11 13:27:15 -0300

    ultima capacitacion

--- src/controller.py
+++ src/controller.py
@@ -2,6 +2,7 @@
 
 import rospy #importar ros para python
 from std_msgs.msg import String, Int32 # importar mensajes de ROS tipo String y tipo Int32
+from duckietown_msgs.msg import Twist2DStamped
 from geometry_msgs.msg import Twist, Point# importar mensajes de ROS tipo geometry / Twist
 from sensor_msgs.msg import Image # importar mensajes de ROS tipo Image
 import cv2 # importar libreria opencv
@@ -15,24 +16,27 @@ class Template(object):
 		super(Template, self).__init__()
 		self.args = args
 		self.sub = rospy.Subscriber("/duckiebot/posicionPato",Point,self.procesar_dis)
-		self.sub= rospy.Suscriber("/duckiebot/filtro", Twist2DStamped,self.procesar_twist)
-		self.publisher = rospy.Publisher("/duckiebot/wheels_driver_node/car_cmd", Twist2DStamped, queue_size = "x")
+		self.sub1= rospy.Subscriber("/duckiebot/filtro", Twist2DStamped,self.procesar_twist)
+		self.publisher = rospy.Publisher("/duckiebot/wheels_driver_node/car_cmd", Twist2DStamped, queue_size = 1)
+		self.dz = 300
 	#def publicar(self):
 
 	#def callback(self,msg):
 
 	def procesar_dis(self, dis):
-		if dis.z<=15:
-		#procesar_twist(self, self.twist):
-		   self.twist.v=0
-		   self.twist.omega=0
-		   self.publisher.publish(self.twist)
+		self.dz=dis.z
+	def procesar_twist(self, twist):
+		if self.dz<=15:
+			twist.v=0
+			twist.omega=0
+		self.publisher.publish(twist)
+		
 		   
 
 
 		
 def main():
-	rospy.init_node('test') #creacion y registro del nodo!
+	rospy.init_node('controller') #creacion y registro del nodo!
 
 	obj = Template('args') # Crea un objeto del tipo Template, cuya definicion se encuentra arriba
 
